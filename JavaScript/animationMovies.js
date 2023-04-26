6e136dba207e4121e2f6ea7ec405ebbfe93d36a6
Marcos <TheMatdiez@gmail.com>
2023-04-26 10:36:50 -0300

    movie id optimized

--- JavaScript/animationMovies.js
+++ JavaScript/animationMovies.js
@@ -6,11 +6,10 @@ export const loadAnimationMovies = async () => {
         const data = await res.json()
         let movies = ''
         data.results.slice(0, 12).forEach(movie => {
-            const movieDetailUrl = `movie-detail.html?id=${movie.id}`
             movies += `
                 <div class="item">   
                     <div class="border-0">
-                        <a href="./Detail/detailPage.html?id=${movieDetailUrl}">
+                        <a href="./Detail/detailPage.html?id=${movie.id}">
                             <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="img" class="card-img-top">
                         </a>    
                         <div class="card-body text-center">
